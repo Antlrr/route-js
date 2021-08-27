e95ddf5d1f4819e6ccdff98a2a6acf9fa2667a33
SMOLKEYS <jiroshimonis@gmail.com>
2021-08-27 14:00:51 +0800

    ak

--- scripts/weaponry/weapons.js
+++ scripts/weaponry/weapons.js
@@ -1,20 +1,5 @@
 
-const thepolyceswrath = extend(Weapon, {
-  reload: 1,
-  top: true,
-  mirror: false,
-  shots: 1,
-  rotate: false,
-  inaccuracy: 0,
-  x: 0,
-  y: 0,
-  shootStatus: StatusEffects.overclock,
-  shootStatusDuration: 180,
-  recoil: 0,
-  shootSound: Sounds.laser,
-  shootType: thepolyceswrathproj,
-});
-//hopefully accurate
+//frags go first, bullets go second, weapons go third, bullet setters go fourth, and the exporter goes last
 const thepolyceswrathprojfrag = extend(LaserBulletType, {
   damage: 43000,
   lifetime: 16,
@@ -35,6 +20,22 @@ const thepolyceswrathproj = extend(EmpBulletType, {
   buildingDamageMultiplier: 10000,
   fragBullets: 10,
   fragBullet: thepolyceswrathprojfrag,
+})
+
+const thepolyceswrath = extend(Weapon, {
+  reload: 1,
+  top: true,
+  mirror: false,
+  shots: 1,
+  rotate: false,
+  inaccuracy: 0,
+  x: 0,
+  y: 0,
+  shootStatus: StatusEffects.overclock,
+  shootStatusDuration: 180,
+  recoil: 0,
+  shootSound: Sounds.laser,
+  shootType: thepolyceswrathproj,
 });
 
 thepolyceswrathproj.bullet = thepolyceswrathproj;
